ec8eaa1916c37a953329213b8af45ac1fd4be87d
mikel <mikelmazuecos@gmail.com>
2021-11-23 19:20:54 +0100

    subo ejercicio juego piedra,papel,tijera

--- ejercicios/tema 3/worksheet 5/ejercicioJuego.js
+++ ejercicios/tema 3/worksheet 5/ejercicioJuego.js
@@ -1,58 +1,100 @@
-class jugador{
+class Jugador{
     constructor(nombre){
         this.nombre = nombre;
         this.victorias = 0;
         this.derrotas = 0;
         this.mano = "";
     }
-
-    jugar(){
-        this.mano = prompt(this.nombre + " Elige piedra, papel o tijera");
-        return this.mano;
-    }
 }
 
-class juego{
+class Juego{
     constructor(jugador1,jugador2) {
         this.jugador1 = jugador1;
         this.jugador2 = jugador2;
         this.estado = "partida sin empezar";
     }
 
-    jugar(){
-        console.log("Empieza la partida");
+    resultado(){
         this.estado = "partida empezada";
-        console.log("Turno del jugador 1: ")
+        p = document.createElement('p');
 
-        while(manoJugador1 == manoJugador2){
-
-        var manoJugador1 = this.jugador1.jugar();
-        var manoJugador2 = this.jugador2.jugar();
-        console.log("jugador 1 sacó " + manoJugador1);
-        console.log("jugador 2 sacó " + manoJugador2);
-        
-        if(manoJugador1 == manoJugador2)
-            console.log("empate");
-
-        else if((manoJugador1 == "piedra" && manoJugador2 == "papel") || 
-                (manoJugador1 == "tijera" && manoJugador2 == "piedra") || 
-                (manoJugador1 == "papel" && manoJugador2 == "tijera")){
-            console.log("gana jugador2");
-            this.jugador2.victorias += 1;
-            this.jugador1.derrotas += 1;}
-
-            else if((manoJugador1 == "piedra" && manoJugador2 == "tijera") || 
-                    (manoJugador1 == "tijera" && manoJugador2 == "papel") || 
-                    (manoJugador1 == "papel" && manoJugador2 == "piedra")){
-                console.log("gana jugador1");
-                this.jugador1.victorias += 1;
-                this.jugador2.derrotas += 1;
-            }
+        if(jugadorPrimero.mano == jugadorSegundo.mano){
+            p.innerHTML = "empate";
+            document.getElementById("resultado").appendChild(p);
         }
+        else if((jugadorPrimero.mano == "piedra" && jugadorSegundo.mano == "papel") || 
+                (jugadorPrimero.mano == "tijera" && jugadorSegundo.mano == "piedra") || 
+                (jugadorPrimero.mano == "papel" && jugadorSegundo.mano == "tijera")){
+            p.innerHTML = "gana " + jugadorSegundo.nombre;
+            document.getElementById("resultado").appendChild(p);
+            jugadorSegundo.victorias += 1;
+            jugadorPrimero.derrotas += 1;
+            document.getElementById("1").innerHTML = jugadorPrimero.nombre.bold() + " => Victorias: " + jugadorPrimero.victorias + " | " + "Derrotas: " + jugadorPrimero.derrotas
+            document.getElementById("2").innerHTML = jugadorSegundo.nombre.bold() + " => Victorias: " + jugadorSegundo.victorias + " | " + "Derrotas: " + jugadorSegundo.derrotas
+            }
+
+            else if((jugadorPrimero.mano == "piedra" && jugadorSegundo.mano == "tijera") || 
+                    (jugadorPrimero.mano == "tijera" && jugadorSegundo.mano == "papel") || 
+                    (jugadorPrimero.mano == "papel" && jugadorSegundo.mano == "piedra")){
+                p.innerHTML = "gana " + jugadorPrimero.nombre;
+                document.getElementById("resultado").appendChild(p);
+                jugadorPrimero.victorias += 1;
+                jugadorSegundo.derrotas += 1;
+                document.getElementById("1").innerHTML = jugadorPrimero.nombre.bold() + " => Victorias: " + jugadorPrimero.victorias + " | " + "Derrotas: " + jugadorPrimero.derrotas
+                document.getElementById("2").innerHTML = jugadorSegundo.nombre.bold() + " => Victorias: " + jugadorSegundo.victorias + " | " + "Derrotas: " + jugadorSegundo.derrotas
+            }
         this.estado = "partida terminada";
     }
 }
 
-jugador1 = new jugador("Mikel");
-jugador2 = new jugador("Jose");
-juego1 = new juego(jugador1,jugador2);
+var p;
+var juego;
+var jugadorPrimero;
+var jugadorSegundo;
+
+function crearJugadores(){
+    var nombre1 = document.getElementById("nombre1").value;
+    jugadorPrimero = new Jugador(nombre1);
+    var nombre2 = document.getElementById("nombre2").value;
+    jugadorSegundo = new Jugador(nombre2);
+    juego = new Juego(jugadorPrimero,jugadorSegundo);
+    document.getElementById("1").innerHTML = jugadorPrimero.nombre.bold() + " => Victorias: " + jugadorPrimero.victorias + " | " + "Derrotas: " + jugadorPrimero.derrotas
+    document.getElementById("2").innerHTML = jugadorSegundo.nombre.bold() + " => Victorias: " + jugadorSegundo.victorias + " | " + "Derrotas: " + jugadorSegundo.derrotas
+}
+
+function manoJugador1Piedra(){
+    jugadorPrimero.mano = "piedra";
+}
+function manoJugador1Papel(){
+    jugadorPrimero.mano = "papel";
+}
+function manoJugador1Tijera(){
+    jugadorPrimero.mano = "tijera";
+}
+function manoJugador2Piedra(){
+    jugadorSegundo.mano = "piedra";
+}
+function manoJugador2Papel(){
+    jugadorSegundo.mano = "papel";
+}
+function manoJugador2Tijera(){
+    jugadorSegundo.mano = "tijera";
+}
+
+
+function resultado(){
+    juego.resultado();
+}
+
+function inicia(){
+    document.getElementById("crearJugadores1").addEventListener('click',crearJugadores);
+    document.getElementById("jugar").addEventListener('click',resultado);
+    document.getElementById("piedra1").addEventListener('click',manoJugador1Piedra);
+    document.getElementById("papel1").addEventListener('click',manoJugador1Papel);
+    document.getElementById("tijera1").addEventListener('click',manoJugador1Tijera);
+    document.getElementById("piedra2").addEventListener('click',manoJugador2Piedra);
+    document.getElementById("papel2").addEventListener('click',manoJugador2Papel);
+    document.getElementById("tijera2").addEventListener('click',manoJugador2Tijera);
+}
+
+window.addEventListener('load',inicia);
