5ba353f12938d60bb915f328c1e7ca96e6ca3491
mikel <mikelmazuecos@gmail.com>
2021-12-01 17:28:29 +0100

    subo ejercicio ping pong

---
+++ ejercicios/tema 5/juego ping pong/clase_bola.js
@@ -0,0 +1,76 @@
+export class bola{
+    constructor(radio, posicionX, posicionY, velocidadX, velocidadY){
+        this.radio = radio;
+        this.x = posicionX;
+        this.y = posicionY;
+        this.vX = velocidadX;
+        this.vY = velocidadY;
+        this.xI = posicionX;
+        this.yI = posicionY;
+        this.marcadorIz = 0
+        this.marcadorDe = 0
+    }
+
+    reset(){
+        this.x = this.xI;
+        this.y = this.yI;
+    }
+
+    mueve(anchoContenedor, altoContenedor)
+    {
+
+        this.x += this.vX;
+        this.y += this.vY;
+        
+        // Comprobamos ahora si está fuera de los límites
+        // Eje X
+        if (this.x<=0)
+        {
+            this.marcadorIz += 1
+            this.reset();
+            document.getElementById("marcador").innerHTML = `${this.marcadorDe} : ${this.marcadorIz}`
+
+        }
+
+        if(this.x >= anchoContenedor){
+            this.marcadorDe += 1
+            this.reset();
+            document.getElementById("marcador").innerHTML = `${this.marcadorDe} : ${this.marcadorIz}`
+        }
+        
+        // Eje Y
+        if (this.y-this.radio <=0 || this.y+this.radio >= altoContenedor )
+        {
+            this.vY *= -1;
+            this.y  = (this.y-this.radio<=0) ? this.radio: altoContenedor-this.radio;
+        }
+    }
+
+    chocaDerecha(barra){
+        let posAnteriorX = this.x;
+        let posAnteriorY = this.y;
+
+        if((barra.x<=this.x+this.radio) && (!(this.y+this.radio <= barra.y) && !(this.y-this.radio>=barra.y+barra.largo))){
+            this.vX *=-1;
+
+            this.x = posAnteriorX - 10;
+            this.y = posAnteriorY;
+        }
+    }
+
+    chocaIzquierda(barra){
+        let posAnteriorX = this.x;
+        let posAnteriorY = this.y;
+
+        if((barra.x+barra.ancho>=this.x-this.radio) && (!(this.y+this.radio <= barra.y) && !(this.y-this.radio>=barra.y+barra.largo))){
+            this.vX *=-1;
+
+            this.x = posAnteriorX + 10;
+            this.y = posAnteriorY;
+        }
+
+        if((this.y+this.radio >= barra.y || this.y-this.radio <= barra.y+barra.largo) && (this.x-this.radio <= barra.x+barra.ancho)){
+            this.vy *= -1;
+        }
+    }
+}
---
+++ ejercicios/tema 5/juego ping pong/index.js
@@ -0,0 +1,78 @@
+import{bola} from './clase_bola.js'
+import{interfaz_bola} from './clase_interfaz_bola.js'
+import{barra} from './clase_barra.js'
+import{interfaz_barra} from './clase interfaz_barra.js'
+
+function aleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function colorAleatorio()
+{
+    return "#"+ Math.floor(Math.random()*16777215).toString(16);
+}
+
+var svg; 
+var tamanoSVG;
+var elId;
+var bola1;
+var bola_moviendo;
+var barra1;
+var barra2;
+var barra_moviendose1;
+var barra_moviendose2;
+
+window.onload = () =>
+{
+    svg = document.getElementById("panel");
+
+    bola1 = new bola(25, 400, 400, 10, 10);
+    bola_moviendo = new interfaz_bola(bola1,'black',svg)
+
+    barra1 = new barra(10,200,30,100)
+    barra2 = new barra(1300,200,30,100)
+
+    barra_moviendose1 = new interfaz_barra(barra1,svg)
+    barra_moviendose2 = new interfaz_barra(barra2,svg)
+
+    window.addEventListener('keydown',moverBarra)
+
+    elId = window.requestAnimationFrame(loop);
+}
+
+function loop()
+  {
+    tamanoSVG = svg.getBoundingClientRect();
+
+    bola_moviendo.bola.mueve(tamanoSVG.width, tamanoSVG.height,barra1,barra2);
+    bola1.chocaDerecha(barra2)
+    bola1.chocaIzquierda(barra1)
+    bola_moviendo.dibuja()
+
+    barra_moviendose1.dibuja()
+    barra_moviendose2.dibuja()
+
+    elId = window.requestAnimationFrame(loop);
+}
+
+function moverBarra(e) {
+    if(e.key == 'p'){
+        barra2.mueve(tamanoSVG.height, -barra2.velocidad)
+        barra_moviendose2.dibuja()
+    }
+
+    else if(e.key == 'l'){
+        barra2.mueve(tamanoSVG.height, barra2.velocidad)
+        barra_moviendose2.dibuja()
+    }
+
+    else if(e.key == 'q'){
+        barra1.mueve(tamanoSVG.height, -barra1.velocidad)
+        barra_moviendose1.dibuja()
+    }
+
+    else if(e.key == 'a'){
+        barra1.mueve(tamanoSVG.height, barra1.velocidad)
+        barra_moviendose1.dibuja()
+    }
+}
